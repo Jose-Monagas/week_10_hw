1fb0c729a9c9f026be1c466569798e8a5256e129
Jose <josemonagas212@gmail.com>
2023-06-10 00:45:58 -0400

    theoretical algorithm

---
+++ theoretical.js
@@ -0,0 +1,36 @@
+function spiralWhore(n) {
+  const arr = Array(n)
+    .fill()
+    .map(() => Array(n));
+
+  let val = 1; // Current value to be filled in the matrix
+  let t = 0; // Top boundary index
+  let b = n - 1; // Bottom boundary index
+  let l = 0; // Left boundary index
+  let r = n - 1; // Right boundary index
+
+  while (l <= r && t <= b) {
+    for (j = l; j >= l && j <= r; j++) {
+      arr[t][j] = val;
+      val++;
+    }
+    t++;
+    for (i = t; i >= t && i <= b; i++) {
+      arr[i][r] = val;
+      val++;
+    }
+    r--;
+    for (j = r; j >= l && j <= r; j--) {
+      arr[b][j] = val;
+      val++;
+    }
+    b--;
+    for (i = b; i >= t && i <= b; i--) {
+      arr[i][l] = val;
+      val++;
+    }
+    l++;
+  }
+  return arr;
+}
+console.log(spiralWhore(4));
